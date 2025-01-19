4d16fd348ad91264afed4fa5fa392a46906d7565
Cotran <shch0516@naver.com>
2025-01-19 22:59:43 +0900

    set keywords

--- tokenizer.py
+++ tokenizer.py
@@ -5,7 +5,7 @@ import re
 TOKEN_TYPES = [
   ('NUMBER', r'\d+'),                                          # 정수
   ('IDENTIFIER', r'[가-힣\w]+'),                        	     # 식별자 (한글 및 영문자)
-  ('KEYWORD', r'\b(키워드)\b'),  															 # 한국어 키워드
+  ('KEYWORD', r'\b(거짓|없음|참|그리고|라고|참인가|비동기|기다림|종료|모음|건너뛰기|함수|삭제|혹은|아니고만약|아니면|모두아니면|예외|무조건|동안|에서|전역|만약|불러오기|안에|같은가|람다|비지역|부정|또는|넘어가기|예외발생|반환|시도|까지|함께|생성값)\b'),  					# 한국어 키워드
   ('OPERATOR', r'[=+\-*/<>!]+'),                               # 연산자
   ('PARENTHESIS', r'[(){}[\]]'),                               # 괄호
   ('COMMA', r','),                                          	 # 쉼표
@@ -15,7 +15,7 @@ TOKEN_TYPES = [
   ('WHITESPACE', r'\s+'),                                   	 # 공백
 ]
 
-
+# Functions
 def tokenize(code):
   tokens = []
   while code:
